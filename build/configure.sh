4849cf2636277c0e143b26c6d5a973b9ca51b661
José Tobias <jose.tobias@outlook.com>
2018-06-26 12:31:18 -0300

    added new commands to 'configure' script

--- build/configure.sh
+++ build/configure.sh
@@ -4,6 +4,8 @@
 [ $UID != 0 ] && exec sudo bash "$0" "$@"
 
 # installing dependencies
+apt install -y openjdk-8-jdk
+apt install -y python-mysqldb
 apt install -y python3
 apt install -y python3-dev
 apt install -y python3-venv
@@ -11,7 +13,6 @@ apt install -y python3-pip
 apt install -y mysql-client
 apt install -y mysql-server
 apt install -y libmysqlclient-dev
-apt install -y expect
 
 # configuring mysql user, and correcting a plugin problem that has been introduced in version 17.10
 # if you're not using ubuntu 17.10, this will probably not change any configuration
@@ -19,7 +20,7 @@ echo -en "\n[root] root password for mysql:"
 read -s rpw
 
 # making needed changes to mysql
-mysql -u root -p${rpw} -e "create database ctsp;create user 'ctsp'@'localhost' identified by '123456';grant all on ctsp.* to 'ctsp'@'localhost';update mysql.user set plugin='' where User='ctsp';flush privileges"
+mysql -u root -p${rpw} -e "create database ctsp;create user 'ctsp'@'localhost' identified by '123456';grant all on ctsp.* to 'ctsp'@'localhost';update mysql.user set plugin='mysql_native_password' where User='ctsp';flush privileges"
 
 # creating the virtual env
 python3 -m venv ../venv
@@ -28,12 +29,14 @@ python3 -m venv ../venv
 source ../venv/bin/activate
 
 # installing needed packages
-pip3 install -U wheel 
-pip3 install -U pytz
-pip3 install -U pylint 
-pip3 install -U autopep8 
-pip3 install -U django 
-pip3 install -U mysqlclient
+pip install -U mysql-python
+pip install -U wheel 
+pip install -U pytz
+pip install -U pylint 
+pip install -U autopep8 
+pip install -U django 
+pip install -U mysqlclient
+pip install -U mysql-python
 
 # makemigrations
 python ../project/manage.py makemigrations
